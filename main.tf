05a362d67f0941b272f516762f1864a648b2756c
kratos <kratos@localhost.localdomain>
2021-10-27 23:50:12 -0500

    2a

--- main.tf
+++ main.tf
@@ -11,8 +11,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  access_key = var.aws_access_key_id 
+  secret_key = var.aws_secret_access_key 
 }
 
 resource "aws_s3_bucket" "testbucket"{
